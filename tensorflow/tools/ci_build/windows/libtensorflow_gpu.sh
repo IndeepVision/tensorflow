535db4677c66a29bc548d867ad5008788f6bc85f
Jordi Vallès <38399062+jvallesf@users.noreply.github.com>
2022-09-11 11:18:34 +0200

    Update libtensorflow_gpu.sh

--- tensorflow/tools/ci_build/windows/libtensorflow_gpu.sh
+++ tensorflow/tools/ci_build/windows/libtensorflow_gpu.sh
@@ -36,13 +36,17 @@ run_configure_for_gpu_build
 # build_libtensorflow_tarball in ../builds/libtensorflow.sh
 # cannot be used on Windows since it relies on pkg_tar rules.
 # So we do something special here
- bazel --output_user_root=${TMPDIR} build --config=opt --copt=/arch:AVX --copt=/arch:AVX2 --copt=/std:c++14 --announce_rc \
-   :LICENSE \
-   tensorflow:tensorflow.dll \
-   tensorflow:tensorflow_dll_import_lib \
-   tensorflow/tools/lib_package:clicenses_generate \
-   #tensorflow/java:tensorflow_jni.dll \
-   #tensorflow/tools/lib_package:jnilicenses_generate
+
+# bazel --output_user_root=${TMPDIR} build --config=opt --copt=/arch:AVX --copt=/arch:AVX2 --copt=/fp:fast \
+#     --copt=/std:c++14 --copt=/MD --copt=/O2 --copt=/Ot --copt=/Oi --copt=/GL --copt=/Gy --announce_rc \ --> config=opt should include all these optimizations
+
+bazel --output_user_root=${TMPDIR} build --config=opt --copt=/arch:AVX --copt=/arch:AVX2 --copt=/fp:fast --copt=/std:c++14 --announce_rc \
+    :LICENSE \
+    tensorflow:tensorflow.dll \
+    tensorflow:tensorflow_dll_import_lib \
+    tensorflow/tools/lib_package:clicenses_generate \
+    #tensorflow/java:tensorflow_jni.dll \
+    #tensorflow/tools/lib_package:jnilicenses_generate
 
 # bazel --output_user_root=${TMPDIR} build -c opt --copt=/arch:AVX --announce_rc --config=short_logs \
 #   :LICENSE \
